6456a0182edad232ef73d53dcd3ab187f8ee551a
hankfrank <henryfrank1@yahoo.com>
2019-07-26 13:21:10 -0700

    adding circle.yml and readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a project for the San Diego Code School, 
+
+It is using express/mongoose to access a mongobd database.
+
+Authored by Henry Frank with starter file and repo cloned from San Deigo Code School in beautiful Escondito, CA. 
--- server/app.js
+++ server/app.js
@@ -3,7 +3,9 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost/my-blog', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost/my-blog', { useNewUrlParser: true });
+mongoose.connect('mmongodb://heroku_08cbnq8b:5d5dls76lrh59vonohregjr1tv@ds021741.mlab.com:21741/heroku_08cbnq8b', { useNewUrlParser: true });
+//mongodb://heroku_08cbnq8b:5d5dls76lrh59vonohregjr1tv@ds021741.mlab.com:21741/heroku_08cbnq8b
 mongoose.Promise = Promise;
 
 const app = express();
