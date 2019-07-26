4e0962d1f9f7a6a8682765c3c6216d43df3b2f61
hankfrank <henryfrank1@yahoo.com>
2019-07-26 13:28:11 -0700

    gave listener .listen(process.env.PORT || ) var to allow heroku to work.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
+// const server = require('./app');
+
+// server.listen(8080, function() {
+//   console.log('Server is listening on http://localhost:8080');
+// });
+
 const server = require('./app');
 
-server.listen(8080, function() {
-  console.log('Server is listening on http://localhost:8080');
-});
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => console.log(`server is listening on port: ${PORT} brosef, come check it out`));
